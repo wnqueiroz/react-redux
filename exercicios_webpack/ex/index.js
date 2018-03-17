55a955e0f9861bb2ae488953c49f44f811287320
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-03-17 15:03:59 -0300

    Interpreting the spread operator (babel plugin)

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,4 +1,14 @@
-import Pessoa from './pessoa'
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
 
-const pessoa = new Pessoa('William')
-console.log(pessoa.toString())
+function clone(object) {
+    return { ...object }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta Bic Azul'
+
+console.log(novoProduto, produto)
