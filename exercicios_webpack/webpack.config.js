a4eeebd633e8b2882e85a0023fe43b0e90fb7a40
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-03-17 11:45:29 -0300

    Using the ES15 module system with babel

--- exercicios_webpack/webpack.config.js
+++ exercicios_webpack/webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
     devServer: {
         port: 8080,
         contentBase: './public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["env"]
+                    }
+                }
+            }
+        ]
     }
 }
