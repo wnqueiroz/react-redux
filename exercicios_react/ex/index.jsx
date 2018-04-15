7749a6827b9eb4c1c99ac32e5d7f52c376e7b989
William Queiroz <william@fredbots.com>
2018-04-15 20:34:52 -0300

    Children's Components part.2

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import SilvaFamily  from './silvaFamily'
+import Family  from './family'
+import Member  from './member'
 
 ReactDOM.render(
-    <SilvaFamily/>,
+    <Family lastName='Silva'>
+        <Member name='Guilherme'/>
+    </Family>,
     document.getElementById('app')
 )
