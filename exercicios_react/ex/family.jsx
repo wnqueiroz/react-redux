e67f87f0686ff49676b95bb64e004ef8cb5790c7
William Queiroz <william@fredbots.com>
2018-04-15 20:42:37 -0300

    Passing properties to child components part.1

--- exercicios_react/ex/family.jsx
+++ exercicios_react/ex/family.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default props => (
     <div>
         <h1>Família {props.lastName}</h1>
-        {props.children}
+        {React.cloneElement(props.children, {...props})}
     </div>
 )
