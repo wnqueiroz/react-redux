f2d6346a49dacfd85617146160e2e56ff4a09a40
William Queiroz <william@fredbots.com>
2018-04-15 21:03:00 -0300

    Class component

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family  from './family'
-import Member  from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-    <Family lastName='Silva'>
-        <Member name='Guilherme'/>
-        <Member name='Rafael'/>
-        <Member name='Júlia'/>
-    </Family>,
-    document.getElementById('app')
+    <ClassComponent value="Componente Classe"/>
+   , document.getElementById('app')
 )
