83ebd96c0073d5bfb27a245812c37703eea05aa9
William Queiroz <william@fredbots.com>
2018-04-21 16:19:42 -0300

    Enabling the cors

--- todo-app/backend/src/config/server.js
+++ todo-app/backend/src/config/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const port = 3003
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({
     extended: true
 }))
 server.use(bodyParser.json())
+server.use(allowCors)
 server.listen(port, () => console.log(`BACKEND is running on http://localhost:${port}`))
 
 module.exports = server
