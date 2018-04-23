ebca7d800e4c669cb32c6aecfe4f78863b2bd7d3
William Queiroz <william@fredbots.com>
2018-04-23 11:17:01 -0300

    Page Header component

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, vero!</p>
+        <h2>Missão e Valores</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolore.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, praesentium.</p>
     </div>
 )
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Todo extends React.Component {
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro' />
             </div>
         )
     }
