1a5239155603773edb972debecff9f7e55b50e43
William Queiroz <william@fredbots.com>
2018-04-23 13:33:52 -0300

    Conditional rendering (if)

--- todo-app/frontend/src/template/iconButton.jsx
+++ todo-app/frontend/src/template/iconButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import If from './if'
 
-export default props => {
-    if (props.hide) return null
-
-    return (
+export default props => (
+    <If test={!props.hide}>
         <button className={'btn btn-' + props.style}
             onClick={props.onClick}>
-            <i className={'fa fa-' + props.icon }></i>
+            <i className={'fa fa-' + props.icon}></i>
         </button>
-    )
-}
+    </If>
+)
