3842deccf4d9ba8f652ce7ba93eaed2ced2d49ab
William Queiroz <william@fredbots.com>
2018-04-23 22:30:11 -0300

    Query and task deletion

--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -12,16 +12,28 @@ export default class Todo extends React.Component {
         this.state = { description: '', list: [] }
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
     }
 
     handleAdd() {
         const description = this.state.description
         axios.post(URL, { description })
-            .then(resp => console.log('Funcionou!'))
+            .then(resp => this.refresh())
     }
 
     handleChange(e) {
         this.setState({ ...this.state, description: e.target.value })
+    }
+
+    handleRemove(todo) {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     render() {
@@ -30,8 +42,10 @@ export default class Todo extends React.Component {
                 <PageHeader name='Tarefas' small='Cadastro' />
                 <TodoForm description={this.state.description}
                     handleChange={this.handleChange}
-                    handleAdd={this.handleAdd} />
-                <TodoList />
+                    handleAdd={this.handleAdd}
+                />
+                <TodoList list={this.state.list}
+                    handleRemove={this.handleRemove} />
             </div>
         )
     }
--- todo-app/frontend/src/todo/todoList.jsx
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <h1>Lista</h1>
-)
+export default props => {
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style="danger" icon="trash-o" onClick={() => props.handleRemove(todo)} />
+                </td>
+            </tr>
+        ))
+    }
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
