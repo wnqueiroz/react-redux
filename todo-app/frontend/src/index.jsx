4a02f3c6456c3200c15b0dfc3349541eae1d725c
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-04-26 00:00:08 -0300

    Configuring Redux Dev Tools

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -4,7 +4,11 @@ import App from './main/app'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
-const store = createStore(reducers)
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools)
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
