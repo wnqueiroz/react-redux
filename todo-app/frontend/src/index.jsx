647b1bbfb5818521d25e398086290717335c81f0
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-04-26 22:49:35 -0300

    Applying Middleware (Redux-Promise)

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './main/app'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
+import promise from 'redux-promise'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducers, devTools)
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
