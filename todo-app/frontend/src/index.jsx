b5a4b92c533dee0ebb9a68eddd1fd051c6889ca4
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-04-26 23:08:02 -0300

    Applying Middleware (Redux-Multi)

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -4,11 +4,13 @@ import App from './main/app'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
+
 import promise from 'redux-promise'
+import multi from 'redux-multi'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
--- todo-app/frontend/src/todo/todoActions.jsx
+++ todo-app/frontend/src/todo/todoActions.jsx
@@ -16,9 +16,9 @@ export const search = () => {
 }
 
 export const add = (description) => {
-    const request = axios.post(URL, {description})    
-    return {
-        type: 'TODO_ADDED',
-        payload: request
-    }
+    const request = axios.post(URL, { description })
+    return [
+        { type: 'TODO_ADDED', payload: request },
+        search()
+    ]
 }
