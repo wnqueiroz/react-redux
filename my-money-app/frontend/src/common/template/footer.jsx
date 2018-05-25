0177f0ef496555d3d4536ffb9dc4de84d55af66e
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-05-24 22:17:37 -0300

    Footer Component

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>Copyryght &copy; 2018</strong>
+        <a href="https://github.com/wnqueiroz" target="_blank">Github</a>
+    </footer>
+)
--- my-money-app/frontend/src/main/app.jsx
+++ my-money-app/frontend/src/main/app.jsx
@@ -2,10 +2,14 @@ import '../common/template/dependencies'
 import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
-
+import Footer from '../common/template/footer'
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 )
