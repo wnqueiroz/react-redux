12aa1b8166aeaacfb430f5848232b34dbcacc719
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-05-24 22:29:14 -0300

    Navigation with react-router

--- my-money-app/frontend/src/common/template/footer.jsx
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default props => (
     <footer className="main-footer">
-        <strong>Copyryght &copy; 2018</strong>
+        <strong>Copyryght &copy; 2018 </strong>
         <a href="https://github.com/wnqueiroz" target="_blank">Github</a>
     </footer>
 )
