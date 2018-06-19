8dfc02f9e4dfae5dd0040f22a108f4176ad59e2f
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-06-19 12:34:43 -0300

    Content Title Component

--- my-money-app/frontend/src/dashboard/dashboard.jsx
+++ my-money-app/frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <Content>
+                    Dashboard
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
