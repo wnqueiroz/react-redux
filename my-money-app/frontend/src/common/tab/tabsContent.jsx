70babc5967d7b3ca85cb54998e158c0018e82f9a
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-07-12 20:39:39 -0300

    Structural components of tabs

---
+++ my-money-app/frontend/src/common/tab/tabsContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const tabsContent = ({ children }) => {
+    return (
+        <div className="tab-content">
+            {children}
+        </div>
+    )
+}
+
+export default tabsContent
