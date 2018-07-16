b30a5d27c8ca45e3463f33a90f862ecc7acaa41e
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-07-16 16:33:09 -0300

    Creating action "getList" and reducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import dashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
