4212218f18f4037f35c69446af3bdb593ee24633
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-08-14 20:30:05 -0300

    Field with custom component

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import LabelAndInput from '../common/form/labelAndInput'
+
 class BillingCycleForm extends Component {
     render() {
         const { handleSubmit } = this.props
         return (
             <form action='' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+
+                    <Field name='name'
+                        component={LabelAndInput}
+                        label='Nome'
+                        cols='12 4'
+                        placeholder='Informe o nome'
+                    />
+                    <Field
+                        name='month'
+                        component={LabelAndInput}
+                        label='Mês'
+                        cols='12 4'
+                        placeholder='Informe o mês'
+                        type='number'
+                    />
+                    <Field
+                        name='year'
+                        component={LabelAndInput}
+                        label='Ano'
+                        cols='12 4'
+                        placeholder='Informe o ano'
+                        type='number'
+                    />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
