5436ecf094e97e5981d1b4b7f9bdd4a4f507c0c5
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-10-10 00:38:39 -0300

    Configuring protected and public routes

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require("express");
+const auth = require("./auth");
 
-module.exports = (server) => {
-    const router = express.Router()
+module.exports = server => {
+  // Rotas protegidas por Token JWT
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
 
-    server.use('/api', router)
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
-}
+  protectedApi.use(auth);
+
+  const BillingCycle = require("../api/billingCycle/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  // Rotas abertas
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+
+  const AuthService = require("../api/user/authService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
+};
