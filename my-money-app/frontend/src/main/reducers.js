854d74819a9e11c5568c0886a2f288e0466a6807
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-10-16 18:06:39 -0300

    Created Auth Reducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import dashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
@@ -10,7 +11,8 @@ const rootReducer = combineReducers({
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer,
 })
 
 export default rootReducer
