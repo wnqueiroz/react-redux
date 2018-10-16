e18f5f73adb58acd3e2b18495ab3da55c39d00dd
William Queiroz <williamdenovaisqueiroz@outlook.com>
2018-10-16 19:10:51 -0300

    Renderizing Auth or App component

--- my-money-app/frontend/src/main/routes.jsx
+++ my-money-app/frontend/src/main/routes.jsx
@@ -3,11 +3,11 @@ import { Router, Route, Redirect, IndexRoute, hashHistory } from 'react-router'
 import Dashboard2 from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
-import App from './app'
+import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App} >
+        <Route path='/' component={AuthOrApp} >
             <IndexRoute component={Dashboard2} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
