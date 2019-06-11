dfbeb76f4b1ad53aa940fbc1491e0b374b03e729
William Queiroz <wnqueiroz@outlook.com>
2019-06-10 23:55:18 -0300

    Button component - 02

--- calculator/src/components/Button/index.js
+++ calculator/src/components/Button/index.js
@@ -5,15 +5,31 @@ import PropTypes from 'prop-types';
 import './styles.css';
 
 const propTypes = {
-  label: PropTypes.string
+  label: PropTypes.string,
+  operation: PropTypes.string,
+  double: PropTypes.string,
+  triple: PropTypes.string,
+  onClick: PropTypes.func.isRequired
 };
 
 const defaultProps = {
-  label: null
+  label: null,
+  operation: null,
+  double: null,
+  triple: null
 };
 
-const Button = ({ label }) => (
-  <button type="button" className="button">
+const Button = ({ label, operation, double, triple, onClick }) => (
+  <button
+    type="button"
+    className={`
+      button 
+      ${operation ? 'operation' : ''}
+      ${double ? 'double' : ''}
+      ${triple ? 'triple' : ''}
+    `}
+    onClick={() => onClick && onClick(label)}
+  >
     {label}
   </button>
 );
--- calculator/src/main/Calculator.js
+++ calculator/src/main/Calculator.js
@@ -8,32 +8,44 @@ import Display from '../components/Display';
 export default class Calculator extends Component {
   componentDidMount() {}
 
-  render() {
-    const buttons = [
-      'AC',
-      '/',
-      '7',
-      '8',
-      '9',
-      '*',
-      '4',
-      '5',
-      '6',
-      '-',
-      '1',
-      '2',
-      '3',
-      '+',
-      '0',
-      '.',
-      '='
-    ];
+  buttons = [
+    { label: 'AC', action: this.clearMemory, triple: true },
+    { label: '/', action: this.setOperation, operation: true },
+    { label: '7', action: this.addDigit },
+    { label: '8', action: this.addDigit },
+    { label: '9', action: this.addDigit },
+    { label: '*', action: this.setOperation, operation: true },
+    { label: '4', action: this.addDigit },
+    { label: '5', action: this.addDigit },
+    { label: '6', action: this.addDigit },
+    { label: '-', action: this.setOperation, operation: true },
+    { label: '1', action: this.addDigit },
+    { label: '2', action: this.addDigit },
+    { label: '3', action: this.addDigit },
+    { label: '+', action: this.setOperation, operation: true },
+    { label: '0', action: this.addDigit, double: true },
+    { label: '.', action: this.addDigit },
+    { label: '=', action: this.setOperation, operation: true }
+  ];
 
+  clearMemory = () => {
+    console.log('clear');
+  };
+
+  setOperation = operation => {
+    console.log({ operation });
+  };
+
+  addDigit = n => {
+    console.log({ n });
+  };
+
+  render() {
     return (
       <div className="calculator">
         <Display value="100" />
-        {buttons.map(button => (
-          <Button label={button} />
+        {this.buttons.map(({ label, action, ...others }) => (
+          <Button label={label} onClick={action || null} {...others} />
         ))}
       </div>
     );
